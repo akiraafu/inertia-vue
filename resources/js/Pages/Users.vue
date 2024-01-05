9613ff3e31bc38b8e4582ace4685e635df920c0c
Akira <junrongfu6@gmail.com>
2024-01-05 22:19:40 +0800

    persistent layouts

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,16 +1,17 @@
 <template>
-    <Layout>
-        <h1 class="text-4xl font-bold">Users</h1>
+    <h1 class="text-4xl font-bold">Users</h1>
 
-        <div style="margin-top: 500px">
-            <p>The current time is {{ time }}.</p>
-            <Link href="/users" class="text-blue-500" preserve-scroll>
-                Refresh
-            </Link>
-        </div>
-    </Layout>
+    <div style="margin-top: 500px">
+        <p>The current time is {{ time }}.</p>
+        <Link href="/users" class="text-blue-500" preserve-scroll>
+            Refresh
+        </Link>
+    </div>
 </template>
 
-<script setup>
+<script>
 import Layout from "../Shared/Layout.vue";
+export default {
+    layout: Layout,
+};
 </script>
