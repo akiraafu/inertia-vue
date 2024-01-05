279ac26aad9ec03f89a928b476cf8c466353a216
Akira <junrongfu6@gmail.com>
2024-01-05 22:08:16 +0800

    using the setup syntax sugar

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -11,10 +11,6 @@
     </Layout>
 </template>
 
-<script>
+<script setup>
 import Layout from "../Shared/Layout.vue";
-export default {
-    components: { Layout },
-    props: { time: String },
-};
 </script>
